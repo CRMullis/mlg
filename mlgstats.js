6d4f83c1a3b96d079b6723aa48531e6cbe76c0b4
Collin Mullis <cmullis@saintjoe.edu>
2015-02-23 11:03:03 -0500

    Added File to git hub, Previous changes were: Added all players and top 4 teams to array.

---
+++ mlgstats.js
@@ -0,0 +1,28 @@
+/* Coded by Collin Mullis
+	CMP237 Dataset Project
+	February 18th 2015
+*/
+// Code keeps track of Team Name, Player Name, Kill Death Ratio, Kill Death Percentile, Slayer (Kills per minute *10), and Maps won.
+var mlgstats = [
+'{ "team": "OpTic Gaming", "player": "Scumpii", "K/D": 1.29, "K/D Percentile": 1, "Slayer": 34.82, "Maps": 15}',
+'{ "team": "OpTic Gaming", "player": "Formal", "K/D": 1.27, "K/D Percentile": 10, "Slayer": 32.04, "Maps": 15}',
+'{ "team": "OpTic Gaming", "player": "Crimsix", "K/D": 1.09, "K/D Percentile": 25, "Slayer": 28.45, "Maps": 15}',
+'{ "team": "OpTic Gaming", "player": "Nadeshot", "K/D": 0.92, "K/D Percentile": Below 50 "Slayer": 24.55", "Maps":15}',
+'{ "team": "Denial", "player": "Attach", "K/D": 0.90, "K/D Percentile": 0.90, "Slayer": 26.20, "Maps": 23}',
+'{ "team": "Denial", "player": "Jkap", "K/D": 1.01, "K/D Percentile": 40, "Slayer": 27.09, "Maps": 23}',
+'{ "team": "Denial", "player": "Clayster", "K/D": 1.14, "K/D Percentile": 20, "Slayer": 30.87, "Maps": 23}',
+'{ "team": "Denial", "player": "Replays", "K/D": 0.92, "K/D Percentile": Below 50, "Slayer": 24.18, "Maps": 23}',
+'{ "team": "Rise", "player": "Sender", "K/D": 0.87, "K/D Percentile": Below 50, "Slayer": 26.76, "Maps": 7}',
+'{ "team": "Rise", "player": "Chino", "K/D": 0.95, "K/D Percentile": 50, "Slayer": 31.29, "Maps": 7}',
+'{ "team": "Rise", "player": "Diabolic", "K/D": 0.82, "K/D Percentile": Below 50, "Slayer": 22.64, "Maps": 7}',
+'{ "team": "Rise", "player": "Pacman", "K/D": 0.67, "K/D Percentile": Below 50, "Slayer": 21.41, "Maps": 7}',
+'{ "team": "EnVyUs", "player": "Nameless", "K/D": 1.04, "K/D Percentile": 30, "Slayer": 27.39, "Maps": 13}',
+'{ "team": "EnVyUs", "player": "Saints", "K/D": 1.16, "K/D Percentile": 15, "Slayer": 30,63, "Maps": 13}',
+'{ "team": "EnVyUs", "player": "Zooma", "K/D": 1.00, "K/D Percentile": 45, "Slayer": 28.63, "Maps": 13}',
+'{ "team": "EnVyUs", "player": "Merk", "K/D": 0.78, "K/D Percentile": Below 50, "Slayer": 22.48, "Maps": 13}',
+'{ "team": "FaZe", "player": "Aches", "K/D": 0.84, "K/D Percentile": Below 50, "Slayer": 22.66, "Maps": 16}',
+'{ "team": "FaZe", "player": "Parasite", "K/D": 1.01, "K/D Percentile": 35, "Slayer": 24.98, "Maps": 16}',
+'{ "team": "FaZe", "player": "Enable", "K/D": 1.18, "K/D Percentile": 10, "Slayer": 28.98, "Maps": 16}',
+'{ "team": "FaZe", "player": "Slasher", "K/D": 1.28, "K/D Percentile": 5, "Slayer": 30.42, "Maps": 16}',
+];
+console.log(mlgstats); 
